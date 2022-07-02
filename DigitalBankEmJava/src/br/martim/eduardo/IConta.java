67073dd6be8e9beb011e94b14e3fd6e0fca22fa6
Eduardo Martim <65382981+EduMartim@users.noreply.github.com>
2022-07-02 15:03:39 -0300

    Add files via upload

---
+++ DigitalBankEmJava/src/br/martim/eduardo/IConta.java
@@ -0,0 +1,12 @@
+package br.martim.eduardo;
+
+public interface IConta {
+	
+	void sacar(double valor);
+	
+	void depositar(double valor);
+	
+	void transferir(double valor, IConta contaDestino);
+	
+	void imprimirExtrato();
+}
